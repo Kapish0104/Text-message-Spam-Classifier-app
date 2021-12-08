8bd3f1a8f55668e7061e8897ef2e64636f50c13b
Kapish Shewale <77770354+Kapish0104@users.noreply.github.com>
2021-12-07 23:57:45 -0600

    Initial app

---
+++ SMS-Classifier-App/Python-flask-app/app.py
@@ -0,0 +1,35 @@
+from flask import Flask, request, render_template
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.naive_bayes import MultinomialNB
+import pickle
+
+def classifyMessage(message_text):
+    nb_model = pickle.load(open('naive_bayes_model', 'rb'))
+    vectorizer = pickle.load(open('vectorizer', 'rb'))
+    message_list =[]
+    message_list.append(message_text)
+    message_vector = vectorizer.transform(message_list)
+    prediction = nb_model.predict(message_vector)[0]
+    print(prediction)
+    if prediction == 0:
+        return 'THE MESSAGE IS NOT A SPAM'
+    else:
+        return 'THE MESSAGE IS A SPAM'
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'GET':
+        message = " "
+        return render_template('index.html')
+    if request.method == 'POST':
+        message = request.form.get('msg')
+        prediction = classifyMessage(message)
+        return render_template('index.html', message= prediction)
+
+if __name__ == '__main__':
+    print("Welcome to my app")
+    app.run(host='0.0.0.0', port=81)
+
+
